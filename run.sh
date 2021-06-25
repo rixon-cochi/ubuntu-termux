f6ba5cfbb71f921a5e03bb37cf4625ff9099516b
Tech cochi (HYDRA) <61864331+rixon-cochi@users.noreply.github.com>
2021-06-26 02:14:37 +0530

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#tech cochi
+#clear
+#2021SA26155AM
+clear
+echo ""
+xdg-open https://youtube.com/c/HYDRAGAMING4U
+sleep 20
+clear
+
+apt update -y
+apt upgrade -y
+apt install git -y
+apt install wget -y
+apt install proot -y
+clear
+sleep 3
+bash logo.sh
+bash ubuntu.sh
+
